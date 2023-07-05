f96c085965f72bbc17abe5f1d89e3e37089f8700
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 13:24:59 +0200

    add template and script to try test for print in dom arrays content

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,66 @@
 <script>
+import axios from '../node_modules/axios';
 import { store } from './data/data'
+const uri = 'https://api.themoviedb.org/3/search/';
+const keyApi = '87c04791798b475aab0066ddfb567b5f';
 export default {
   data() {
-    return store
-  }
+    return {
+      store,
+      userSearch: '',
+    }
+  },
+
+  computed: {
+    currentMovieUserSearch() {
+      return `${uri}movie?api_key=${keyApi}&query=${this.userSearch}&language=it-IT`;
+    },
+    currentSeriesUserSearch() {
+      return `${uri}tv?api_key=${keyApi}&query=${this.userSearch}&language=it-IT`;
+    }
+  },
+  methods: {
+    getMovieContent(target) {
+      axios.get(target).then(res => {
+        store.Movies = res.data.results;
+      })
+    },
+
+    getSeriesContent(target) {
+      axios.get(target).then(res => {
+        store.Series = res.data.results;
+      })
+    },
+  },
 }
 </script>
 
-<template></template>
+<template>
+  <input type="text" v-model.trim="userSearch">
+  <button @click="getMovieContent(currentMovieUserSearch),
+    getSeriesContent(currentSeriesUserSearch)">Cerca
+  </button>
+
+  <h2>film</h2>
+  <ul>
+    <li v-for="movie in store.Movies" :key="movie.id">
+      <h5>{{ movie.title }}</h5>
+      <h5>{{ movie.original_title }}</h5>
+      <h5>{{ movie.original_language }}</h5>
+      <h5>{{ movie.vote_average }}</h5>
+    </li>
+  </ul>
+  <hr>
+  <h2>serie</h2>
+
+  <ul>
+    <li v-for="episode in store.Series" :key="episode.id">
+      <h5>{{ episode.name }}</h5>
+      <h5>{{ episode.original_name }}</h5>
+      <h5>{{ episode.original_language }}</h5>
+      <h5>{{ episode.vote_average }}</h5>
+    </li>
+  </ul>
+</template>
 
 <style scoped></style>
