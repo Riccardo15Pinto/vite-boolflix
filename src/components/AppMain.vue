12daac900a56b19444dc97ade72b8cdf872eafea
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 19:12:50 +0200

    set appmain content

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,31 @@
 <script>
+import { store } from '../data/data'
 import CardMain from './CardMain.vue';
 export default {
     components: { CardMain },
-
+    data() {
+        return store
+    }
 }
 </script>
 
 <template>
-    <CardMain />
+    <main>
+        <h2>FILM</h2>
+        <ul>
+            <CardMain v-for="movie in Movies" :key="movie.id" :title="movie.title" :original_title="movie.original_title"
+                :image="movie.poster_path" :vote="movie.vote_average" :id="movie.id"
+                :original_language="movie.original_language" />
+        </ul>
+
+        <h2>SERIE</h2>
+        <ul>
+            <CardMain v-for="episode in Series" :key="episode.id" :title="episode.name"
+                :original_title="episode.original_name" :image="episode.poster_path" :vote="episode.vote_average"
+                :id="episode.id" :original_language="episode.original_language" />
+        </ul>
+    </main>
 </template>
 
 <style scoped></style>
+
