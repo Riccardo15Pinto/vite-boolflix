e258b98ffc3a15829f1fb6f8afe88d33e19a09da
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 16:27:33 +0200

    import new components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 import axios from '../node_modules/axios';
 import { store } from './data/data'
 const uri = 'https://api.themoviedb.org/3/search/';
 const keyApi = '87c04791798b475aab0066ddfb567b5f';
 export default {
+  component: { AppHeader, AppMain },
   data() {
     return {
       store,
@@ -43,7 +46,8 @@ export default {
 
     getImagePoster(object) {
       return `https://image.tmdb.org/t/p/w342${object}`;
-    }
+    },
+
   },
 }
 </script>
@@ -59,12 +63,20 @@ export default {
     <li v-for="movie in store.Movies" :key="movie.id">
       <h5>{{ movie.title }}</h5>
       <h5>{{ movie.original_title }}</h5>
-      <img v-if="movie.backdrop_path" :src="getImagePoster(movie.backdrop_path)" alt="">
-      <img v-if="!movie.backdrop_path" src="https://arice.leggere.it/arimg.ashx?e=9788871191959&k=gb&w=300&h=500" alt="">
+      <div>
+        <img v-if="movie.backdrop_path" :src="getImagePoster(movie.backdrop_path)" alt="">
+        <img v-if="!movie.backdrop_path" src="https://arice.leggere.it/arimg.ashx?e=9788871191959&k=gb&w=300&h=500"
+          alt="">
+      </div>
       <h5>{{ getCeilAvarage(movie.vote_average) }}</h5>
+      <div>
+        <FontAwsomeIcon :icon="['fas', 'star']" />
+      </div>
       <h5 v-show="movie.original_language != 'en' && movie.original_language != 'it'">{{ movie.original_language }}</h5>
-      <img v-if="movie.original_language == 'en' || movie.original_language == 'it'"
-        :src="getImagePath(movie.original_language)" alt="">
+      <div>
+        <img v-if="movie.original_language == 'en' || movie.original_language == 'it'"
+          :src="getImagePath(movie.original_language)" alt="">
+      </div>
     </li>
   </ul>
   <hr>
@@ -74,17 +86,23 @@ export default {
     <li v-for="episode in store.Series" :key="episode.id">
       <h5>{{ episode.name }}</h5>
       <h5>{{ episode.original_name }}</h5>
-      <img v-if="episode.backdrop_path" :src="getImagePoster(episode.backdrop_path)" alt="">
-      <img v-if="!episode.backdrop_path" src="https://arice.leggere.it/arimg.ashx?e=9788871191959&k=gb&w=300&h=500"
-        alt="">
+      <div>
+        <img v-if="episode.backdrop_path" :src="getImagePoster(episode.backdrop_path)" alt="">
+        <img v-if="!episode.backdrop_path" src="https://arice.leggere.it/arimg.ashx?e=9788871191959&k=gb&w=300&h=500"
+          alt="">
+      </div>
       <h5>{{ getCeilAvarage(episode.vote_average) }}</h5>
+      <div>
+        <FontAwsomeIcon :icon="['fas', 'star']" />
+      </div>
       <h5 v-show="episode.original_language != 'en' && episode.original_language != 'it'">{{ episode.original_language }}
       </h5>
-      <img v-if="episode.original_language == 'en' || episode.original_language == 'it'"
-        :src="getImagePath(episode.original_language)" alt="">
+      <div>
+        <img v-if="episode.original_language == 'en' || episode.original_language == 'it'"
+          :src="getImagePath(episode.original_language)" alt="">
+      </div>
     </li>
   </ul>
-  <FontAwsomeIcon :icon="['fas', 'star']" />
 </template>
 
 <style scoped></style>
