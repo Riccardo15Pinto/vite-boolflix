0758916465aa3d691ae56f9ee162faeca9c18686
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 15:58:39 +0200

    fix line 10

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 library.add(faStar)
 
 createApp(App)
