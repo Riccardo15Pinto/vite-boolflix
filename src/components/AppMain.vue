069f5c3ddbd7f8dc3d89fd63619c65c6d625d90b
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 16:27:09 +0200

    add appmain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+import CardMain from './components/CardMain.vue';
+export default {
+    components: { CardMain },
+
+}
+</script>
+
+<template>
+    <CardMain />/>
+</template>
+
+<style scoped></style>
