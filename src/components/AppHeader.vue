aa433379770cbede9b11f5da501ac42554a626bc
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 16:27:02 +0200

    add appheader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<script>
+import InputFilter from '../components/InputFilter.vue';
+export default {
+    components: { InputFilter },
+}
+</script>
+
+<template>
+    <InputFilter />
+</template>
+
+<style scoped></style>
