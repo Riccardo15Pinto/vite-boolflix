40a27fd24f96ea6eeda03a62bd2f27ee61106094
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 15:12:00 +0200

    add and use get image Poster

--- src/App.vue
+++ src/App.vue
@@ -39,6 +39,10 @@ export default {
 
     getCeilAvarage(object) {
       return Math.ceil(object)
+    },
+
+    getImagePoster(object) {
+      return `https://image.tmdb.org/t/p/w342${object}`;
     }
   },
 }
@@ -55,9 +59,12 @@ export default {
     <li v-for="movie in store.Movies" :key="movie.id">
       <h5>{{ movie.title }}</h5>
       <h5>{{ movie.original_title }}</h5>
-      <h5 v-show="movie.original_language != 'en' && movie.original_language != 'it'">{{ movie.original_language }}</h5>
+      <img v-if="movie.backdrop_path" :src="getImagePoster(movie.backdrop_path)" alt="">
+      <img v-if="!movie.backdrop_path" src="https://arice.leggere.it/arimg.ashx?e=9788871191959&k=gb&w=300&h=500" alt="">
       <h5>{{ getCeilAvarage(movie.vote_average) }}</h5>
-      <img :src="getImagePath(movie.original_language)" alt="">
+      <h5 v-show="movie.original_language != 'en' && movie.original_language != 'it'">{{ movie.original_language }}</h5>
+      <img v-if="movie.original_language == 'en' || movie.original_language == 'it'"
+        :src="getImagePath(movie.original_language)" alt="">
     </li>
   </ul>
   <hr>
@@ -67,11 +74,14 @@ export default {
     <li v-for="episode in store.Series" :key="episode.id">
       <h5>{{ episode.name }}</h5>
       <h5>{{ episode.original_name }}</h5>
-      <h5 v-show="episode.original_language != 'en' && episode.original_language != 'it'">{{ episode.original_language
-      }}
-      </h5>
+      <img v-if="episode.backdrop_path" :src="getImagePoster(episode.backdrop_path)" alt="">
+      <img v-if="!episode.backdrop_path" src="https://arice.leggere.it/arimg.ashx?e=9788871191959&k=gb&w=300&h=500"
+        alt="">
       <h5>{{ getCeilAvarage(episode.vote_average) }}</h5>
-      <img :src="getImagePath(episode.original_language)" alt="">
+      <h5 v-show="episode.original_language != 'en' && episode.original_language != 'it'">{{ episode.original_language }}
+      </h5>
+      <img v-if="episode.original_language == 'en' || episode.original_language == 'it'"
+        :src="getImagePath(episode.original_language)" alt="">
     </li>
   </ul>
 </template>
