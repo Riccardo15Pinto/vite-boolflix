5e9cbff37610426cc8e959377898934cb0261215
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 16:42:36 +0200

    set appheader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,11 +2,20 @@
 import InputFilter from '../components/InputFilter.vue';
 export default {
     components: { InputFilter },
+    emits: ['Input-change'],
+    methods: {
+        getInputChange(object) {
+            this.$emit('Input-change', object)
+        }
+    }
 }
 </script>
 
 <template>
-    <InputFilter />
+    <header>
+        <h1>BOOLFLIX</h1>
+        <InputFilter @input-change="getInputChange" />
+    </header>
 </template>
 
 <style scoped></style>
