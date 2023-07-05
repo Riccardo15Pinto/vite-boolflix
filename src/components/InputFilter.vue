7717d79a48a3b865dc3bd4b04f5b0d367dfdc7b2
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 16:42:28 +0200

    SET INPUTfILTER

--- src/components/InputFilter.vue
+++ src/components/InputFilter.vue
@@ -1,10 +1,20 @@
 <script>
 
 export default {
-
+    data() {
+        return {
+            userSearch: '',
+        }
+    },
+    emits: ['Input-change'],
 }
 </script>
 
-<template></template>
+<template>
+    <div>
+        <input type="text" v-model.trim="userSearch">
+        <button @click="$emit('Input-change', userSearch)">Cerca</button>
+    </div>
+</template>
 
 <style scoped></style>
