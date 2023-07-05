64daba8296226a076165f67237c05aa858a11060
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-05 15:55:38 +0200

    rec fontawsome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+library.add(faStar)
+
+createApp(App)
+    .component('FontAwsomeIcon' , FontAwesomeIcon)
+    .mount('#app')
