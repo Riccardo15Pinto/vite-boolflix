7cee3f52b2f82be0d890dc2af7682a091a0e9672
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-06 18:37:21 +0200

    add comment and fix fetchcontnent

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,49 @@
 <script>
+// import header
 import AppHeader from './components/AppHeader.vue';
+// import main
 import AppMain from './components/AppMain.vue';
+// import axios
 import axios from '../node_modules/axios';
+// import store 
 import { store } from './data/data'
+// base uri
 const uri = 'https://api.themoviedb.org/3/search/';
+// my key api
 const keyApi = '87c04791798b475aab0066ddfb567b5f';
 export default {
+  // rec components
   components: { AppHeader, AppMain },
   data() {
-    return { store };
+    // rec store
+    return store
   },
 
   methods: {
-
+    // call data from server
     fetchContent(object, endpoint, destination, target) {
-      axios.get(`${uri}${endpoint}?api_key=${keyApi}&query=${object}&language=it-IT`).then(res => {
-        store[destination] = res.data.results;
-        store[destination].forEach(element => {
-          axios.get(`https://api.themoviedb.org/3/${endpoint}/${element.id}/credits?api_key=${keyApi}`).then(res => {
-            store[target].push(res.data);
-            store[target].forEach((element) => {
-              element.cast.splice(5, element.cast.length - 1);
-            });
-
+      axios
+        .get(
+          `${uri}${endpoint}?api_key=${keyApi}&query=${object}&language=it-IT`
+        )
+        .then((res) => {
+          store[destination] = [];
+          res.data.results.forEach((element) => {
+            axios
+              .get(
+                `https://api.themoviedb.org/3/${endpoint}/${element.id}/credits?api_key=${keyApi}`
+              )
+              .then((res) => {
+                store[target].push(res.data);
+                store[target].forEach((element) => {
+                  element.cast.splice(5, element.cast.length - 1);
+                });
+                store[destination].push(element);
+              });
           });
         });
-      });
     },
-
+    // call two times fetchcontent
     getMediaContent(object) {
       this.fetchContent(object, 'movie', 'Movies', 'CastMovie')
       this.fetchContent(object, 'tv', 'Series', 'CastSeries')
@@ -38,7 +54,10 @@ export default {
 </script>
 
 <template>
+  <!-- header component -->
+  <!-- on click call getMediaContent -->
   <AppHeader @input-change="getMediaContent" />
+  <!-- main component -->
   <AppMain />
 </template>
 
@@ -51,5 +70,21 @@ export default {
 
 body {
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+::-webkit-scrollbar {
+  width: 7px;
+}
+
+::-webkit-scrollbar-track {
+  background: #818181;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #ff0f0f;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #b81919;
 }
 </style>
